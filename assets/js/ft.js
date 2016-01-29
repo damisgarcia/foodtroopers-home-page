643a57a92995532069f1d59f9c8e09a02afdb778
damisgarcia <damisgarcia@gmail.com>
2016-01-29 18:05:01 -0300

    Repair Bug in Flexbox on Safari

--- assets/js/ft.js
+++ assets/js/ft.js
@@ -1,6 +1,11 @@
 $(document).ready(function(e){
   console.log("Ready!")
 
+  // Detected User Browser
+  var device = new MobileDetect(window.navigator.userAgent)
+  // Browser Compatibilty
+  $("body").addClass(device.userAgent().toString().toLowerCase())
+
   $('.ft-device').on('click',function(e){
     if(!$(this).hasClass(".active")){
       $('.ft-device').each(function(index,ele){
